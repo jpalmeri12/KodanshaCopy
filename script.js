2b974b278d44a492e95207cbb40a0ffe54deeaff
mint-cipher <cpumaster930@optonline.net>
2019-09-17 20:59:53 -0400

    Local storage and next button

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 var kanji = [];
+var info = {
+    start: 1,
+    end: 20
+}
 
 $(function () {
+    var newInfo = localStorage.getItem("kodanshacopy");
+    console.log(newInfo);
+    if (newInfo != null) {
+        info = JSON.parse(newInfo);
+        $("#from").val(info.start);
+        $("#to").val(info.end);
+    }
+    else {
+        saveData();
+    }
+    $("#from").val(info.start);
+    $("#to").val(info.end);
     $.ajax("kodansha.txt").done(function (data) {
         console.log("ready");
         kanji = data.split("\n");
         console.log(kanji);
         document.querySelector("#btn").addEventListener("click", copy);
-
+        document.querySelector("#adv").addEventListener("click", advance);
     });
 });
 
@@ -15,9 +31,12 @@ function copy() {
     var str = "";
     var start = parseInt($("#from").val()) - 1;
     var end = parseInt($("#to").val()) - 1;
+    info.start = start + 1;
+    info.end = end + 1;
+    console.log(info);
     for (var i = start; i <= end; i++) {
         if (i >= 0 && i < kanji.length) {
-            str += kanji[i] + "\n";
+            str += `${kanji[i]} (${i+1})\n`;
         }
     }
     str = str.trim();
@@ -26,6 +45,24 @@ function copy() {
     copyText.select();
     document.execCommand("copy");
     iosCopyToClipboard(document.getElementById("copy"));
+    saveData();
+}
+
+function advance() {
+    var start = parseInt($("#from").val());
+    var end = parseInt($("#to").val());
+    var diff = end - start;
+    start = end + 1;
+    end = start + diff;
+    info.start = start;
+    info.end = end;
+    $("#from").val(start);
+    $("#to").val(end);
+    saveData();
+}
+
+function saveData() {
+    localStorage.setItem("kodanshacopy", JSON.stringify(info));
 }
 
 function iosCopyToClipboard(el) {
